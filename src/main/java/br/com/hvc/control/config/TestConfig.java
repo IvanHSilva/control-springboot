e9d8add48d1549890ac9a6e5b8386a51b742ce55
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-02-06 19:49:24 -0300

    OrderStatus enum

--- src/main/java/br/com/hvc/control/config/TestConfig.java
+++ src/main/java/br/com/hvc/control/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.hvc.control.entities.Order;
 import br.com.hvc.control.entities.User;
+import br.com.hvc.control.entities.enums.OrderStatus;
 import br.com.hvc.control.repositories.OrderRepository;
 import br.com.hvc.control.repositories.UserRepository;
 
@@ -31,11 +32,11 @@ public class TestConfig implements CommandLineRunner {
 		User u3 = new User(null, "Yuri Sakamoto", "yuri@gmail.com", "90011-2233", "22222");
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
-		Order o4 = new Order(null, Instant.parse("2019-07-23T16:00:00Z"), u1);
-		Order o5 = new Order(null, Instant.parse("2019-08-01T18:09:05Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u3);
+		Order o4 = new Order(null, Instant.parse("2019-07-23T16:00:00Z"), OrderStatus.DELIVERED, u1);
+		Order o5 = new Order(null, Instant.parse("2019-08-01T18:09:05Z"), OrderStatus.SHIPPED, u2);
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5));
 	}
 }
