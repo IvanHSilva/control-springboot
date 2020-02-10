78cff6d14429ae30f204621e81fc8ae10cb6e7bb
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-02-10 10:41:59 -0300

    Payment, one-to-one association

--- src/main/java/br/com/hvc/control/config/TestConfig.java
+++ src/main/java/br/com/hvc/control/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.hvc.control.entities.Category;
 import br.com.hvc.control.entities.Order;
 import br.com.hvc.control.entities.OrderItem;
+import br.com.hvc.control.entities.Payment;
 import br.com.hvc.control.entities.Product;
 import br.com.hvc.control.entities.User;
 import br.com.hvc.control.entities.enums.OrderStatus;
@@ -88,5 +89,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi7 = new OrderItem(o5, p4, 1, p4.getPrice());
 		OrderItem oi8 = new OrderItem(o5, p3, 1, p3.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4, oi5, oi6, oi7, oi8));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
